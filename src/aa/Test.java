6c4d733d2b9ed7422ba7733bebd88c7be66cc602
lichunfaGoit <18046046775@163.com>
2018-05-04 15:06:50 +0800

    测试测试

--- src/aa/Test.java
+++ src/aa/Test.java
@@ -1,12 +1,15 @@
 package aa;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
 
-		System.out.println("asdfdsfdsf");
-		System.out.println("");
-		System.out.println("你好");
+		Scanner  sc = new Scanner(System.in);
+		System.out.println("请输入========");
+		String next = sc.next();
+		System.out.println("next===="+next);
 	}
 
 }
